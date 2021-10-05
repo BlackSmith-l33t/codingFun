a7f1b4bc99d332a50c8e274559e1ae4728fe68e9
Nobody <nobody133738@gmail.com>
2021-10-05 17:40:20 +0900

    eunm 꼼수 (출처 : 포큐아카데미)

---
+++ enum_littleTrick/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace enum_littleTrick
+{
+    class Program
+    {
+        enum EDirection
+        {
+            North,
+            South,
+            East,
+            West,
+            Max,
+        };
+
+        static void Main(string[] args)
+        {
+            // 어느 함수
+            string[] directions = new string[(int)EDirection.Max];
+            
+            for (int i = 0; i < directions.Length; i++)
+            {
+                // 코드
+            }
+        }
+    }
+}
